8274478f8962a0084e879a5cba099400fc84b999
Tarfprima <granta.150@gmail.com>
2025-07-12 19:49:05 +0300

    Внесены небольшие изменения: Подлючена база данных PosgreSQL Создана одна hmtl страница и написал его ренден в views Добавлены ссылки в urls приложения и проекта

--- ste/stem/models.py
+++ ste/stem/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.utils import timezone
+from datetime import datetime
 from django.contrib.auth.models import User # Возможность дабавить пользователя 
 
-
+# Ранний models.py 
 class Task(models.Model):
     user = models.ForeignKey(User, default=1, on_delete=models.CASCADE) # Добавление пользователя и условия для удаления всех данных.
     name = models.CharField(max_length=255) # заголовок 
     description = models.CharField(max_length=10000) # текстовое поле
-    created = models.DateTimeField(default=timezone.now, blank=True) # поле даты
+    created = models.DateTimeField(default=datetime.now(), blank=True) # Поле даты
     
 
     def __str__(self):
